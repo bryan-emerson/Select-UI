112dd20321bce16a902674c2202109f91f6e1104
Bryan Emerson <bryan.l.emerson@gmail.com>
2022-11-25 17:58:31 -0800

    styles implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Select } from "./Select"
 import './App.css'
 
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fifth", value: 5 }
+]
+
 function App() {
   return (
     <>
-      <Select />
+      <Select options={options}/>
     </>
   )
 }
--- src/select.tsx
+++ src/select.tsx
@@ -14,14 +14,14 @@ type SelectProps = {
 export function Select({ value, onChange, options }: SelectProps) {
   return (
     <>
-      <div className={styles.container}>
+      <div tabIndex={0} className={styles.container}>
         <span className={styles.value}>value</span>
         <button className={styles["clear-btn"]}>&times;</button>
         <div className={styles.divider}></div>
         <div className={styles.caret}></div>
         <ul className={styles.options}>
           {options.map(option=>(
-            <li key={option.value} className={styles.option}>
+            <li key={option.label} className={styles.option}>
               {option.label}
             </li>
           ))}
