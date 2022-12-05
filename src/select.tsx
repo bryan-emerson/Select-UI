0a9382566a7ac524c758d648aeaaef09d63f1325
Bryan Emerson <bryan.l.emerson@gmail.com>
2022-12-05 12:56:06 -0800

    keyboard accessibility implemented

--- src/select.tsx
+++ src/select.tsx
@@ -54,23 +54,40 @@ export function Select({ multiple, value, onChange, options }: SelectProps) {
     const handler = (e: KeyboardEvent) => {
       if (e.target != containerRef.current) return
       switch (e.code) {
-        case "Enter" :
-          case "Space" :
-            setIsOpen(prev => !prev)
+        case "Enter":
+        case "Space":
+          setIsOpen(prev => !prev)
+          if (isOpen) selectOption(options[highlightedIndex])
+          break
+        case "ArrowUp":
+        case "ArrowDown": {
+          if (!isOpen) {
+            setIsOpen(true)
             break
+          }
+
+          const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1)
+          if (newValue >= 0 && newValue < options.length) {
+            setHighlightedIndex(newValue)
+          }
+          break
+        }
+        case "Escape":
+          setIsOpen(false)
+          break
       }
     }
     containerRef.current?.addEventListener("keydown", handler)
 
     return () => {
-    containerRef.current?.removeEventListener("keydown", handler)
+      containerRef.current?.removeEventListener("keydown", handler)
     }
-  }, [])
+  }, [isOpen, highlightedIndex, options])
 
   return (
     <>
       <div
-      ref={containerRef}
+        ref={containerRef}
         onBlur={() => setIsOpen(false)}
         onClick={() => setIsOpen(prev => !prev)}
         tabIndex={0}
@@ -78,27 +95,27 @@ export function Select({ multiple, value, onChange, options }: SelectProps) {
       >
         <span className={styles.value}>
           {multiple
-           ? value.map(v => (
-          <button
-           key={v.value}
-           onClick={e => {
+            ? value.map(v => (
+              <button
+                key={v.value}
+                onClick={e => {
+                  e.stopPropagation()
+                  selectOption(v)
+                }}
+                className={styles["option-badge"]}
+              >
+                {v.label}
+                <span className={styles["remove-btn"]}>&times;</span>
+              </button>
+            ))
+            : value?.label}
+        </span>
+        <button
+          onClick={e => {
             e.stopPropagation()
-            selectOption(v)
+            clearOptions()
           }}
-          className={styles["option-badge"]}
-          >
-            {v.label}
-            <span className={styles["remove-btn"]}>&times;</span>
-            </button>
-        ))
-      : value?.label}
-      </span>
-      <button
-       onClick={e => {
-          e.stopPropagation()
-          clearOptions()
-        }}
-        className={styles["clear-btn"]}>&times;</button>
+          className={styles["clear-btn"]}>&times;</button>
         <div className={styles.divider}></div>
         <div className={styles.caret}></div>
         <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
