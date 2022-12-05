03a5195974a527aeaa9f292f68afe10f12c612e9
Bryan Emerson <bryan.l.emerson@gmail.com>
2022-12-05 11:33:27 -0800

    multi select logic continues

--- src/select.tsx
+++ src/select.tsx
@@ -26,11 +26,19 @@ export function Select({ multiple, value, onChange, options }: SelectProps) {
   const [highlightedIndex, setHighlightedIndex] = useState(0)
 
   function clearOptions() {
-    onChange(undefined)
+    multiple ? onChange([]) : onChange(undefined)
   }
 
   function selectOption(option: SelectOption) {
-    if (option !== value) onChange(option)
+    if (multiple) {
+      if(value.includes(option)) {
+        onChange(value.filter(o => o !== option))
+      } else {
+        onChange([...value, option])
+      }
+    } else {
+      if (option !== value) onChange(option)
+    }
   }
 
   function isOptionSelected(option: SelectOption) {
@@ -49,7 +57,12 @@ export function Select({ multiple, value, onChange, options }: SelectProps) {
         tabIndex={0}
         className={styles.container}
       >
-        <span className={styles.value}>{value?.label}</span>
+        <span className={styles.value}>{multiple ? value.map(v => (
+          <button key={v.value} onClick={e =>{
+            e.stopPropagation()
+            selectOption(v)
+          }}></button>
+        )) : value?.label}</span>
         <button onClick={e => {
           e.stopPropagation()
           clearOptions()
